8570bfe54ddbed944d148358b4dc895f81d1bb1a
lazosee <oseelazaro@gmail.com>
2026-01-17 17:30:10 +0300

    Update /humanize-product cors

--- src/api/humanize-product.ts
+++ src/api/humanize-product.ts
@@ -48,65 +48,58 @@ export class ProductData {
 	}
 }
 
-export default defineHandler({
-	async handler({ req }) {
-		if (req.method.toUpperCase() === 'GET') {
-			return {
-				message: 'Method not allowed',
-				status: 405,
-				usage: {
-					method: 'POST',
-					body: {
-						description_html: 'string',
-						keywords: 'string',
-						prompt: 'string',
-						selected_voice: 'string',
-						specs: 'string',
-						title: 'string',
-					},
+export default defineHandler(async ({ req, res }) => {
+	req.headers.set('Access-Control-Allow-Origin', '*')
+	req.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+	req.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+	req.headers.set('Access-Control-Max-Age', '86400')
+
+	res.headers.set('Access-Control-Allow-Origin', '*')
+	res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+	res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+	res.headers.set('Access-Control-Max-Age', '86400')
+
+	if (req.method.toUpperCase() === 'GET') {
+		return {
+			message: 'Method not allowed',
+			status: 405,
+			usage: {
+				method: 'POST',
+				body: {
+					description_html: 'string',
+					keywords: 'string',
+					prompt: 'string',
+					selected_voice: 'string',
+					specs: 'string',
+					title: 'string',
 				},
-			}
-		} else if (req.method.toUpperCase() === 'POST') {
-			const { prompt, title, specs, description_html, selected_voice, keywords } =
-				(await req.json()) as {
-					prompt: string
-					title: string
-					specs: string
-					description_html: string
-					selected_voice: string
-					keywords: string
-				}
-
-			if (!title || !specs || !keywords || !selected_voice || !description_html) {
-				return { message: 'Provide all required fields', status: 401 }
-			}
-
-			const productData = new ProductData({
-				title: title!,
-				specs: specs!,
-				description_html: description_html!,
-				selected_voice: selected_voice!,
-				keywords: keywords!,
-			})
-			const response = await upcellenceAI.humanizeProduct(prompt, productData)
-			return productSchema.parse(
-				JSON.parse(response.text ?? JSON.stringify({ ...response.candidates?.[0] }))
-			)
+			},
 		}
-	},
-	middleware: [
-		async ({ context, req, res }, next) => {
-			req.headers.set('Access-Control-Allow-Origin', '*')
-			req.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-			req.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-			req.headers.set('Access-Control-Max-Age', '86400')
+	} else if (req.method.toUpperCase() === 'POST') {
+		const { prompt, title, specs, description_html, selected_voice, keywords } =
+			(await req.json()) as {
+				prompt: string
+				title: string
+				specs: string
+				description_html: string
+				selected_voice: string
+				keywords: string
+			}
 
-			res.headers.set('Access-Control-Allow-Origin', '*')
-			res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-			res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-			res.headers.set('Access-Control-Max-Age', '86400')
+		if (!title || !specs || !keywords || !selected_voice || !description_html) {
+			return { message: 'Provide all required fields', status: 401 }
+		}
 
-			return next()
-		},
-	],
+		const productData = new ProductData({
+			title: title!,
+			specs: specs!,
+			description_html: description_html!,
+			selected_voice: selected_voice!,
+			keywords: keywords!,
+		})
+		const response = await upcellenceAI.humanizeProduct(prompt, productData)
+		return productSchema.parse(
+			JSON.parse(response.text ?? JSON.stringify({ ...response.candidates?.[0] }))
+		)
+	}
 })
