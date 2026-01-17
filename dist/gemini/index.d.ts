381950bd9748e297714f165d48e1e04a6eb9922a
lazosee <oseelazaro@gmail.com>
2026-01-17 11:58:27 +0300

    Delete dist

--- dist/gemini/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { UpcellenceAI } from './gemini.mjs';
-export declare const upcellenceAI: UpcellenceAI;
-//# sourceMappingURL=index.d.ts.map
--- dist/gemini/index.js
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv';
-import { UpcellenceAI } from './gemini.mjs';
-config();
-export const upcellenceAI = new UpcellenceAI('gemini-flash-latest', {
-    apiKey: process.env.GEMINI_API_KEY,
-});
-//# sourceMappingURL=index.js.map
