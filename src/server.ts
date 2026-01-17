cf3aba7833f44834e52cc864d8ebd49d8de81b13
lazosee <oseelazaro@gmail.com>
2026-01-17 17:41:54 +0300

    Update server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import { config } from 'dotenv'
 import { H3, serve } from 'h3'
-import humanizeProduct from './api/humanize-product.js'
+import { ProductData } from './api/humanize-product.js'
+import { upcellenceAI } from './gemini/index.js'
+import { productSchema } from './gemini/schema.js'
 
 config()
 
 const app = new H3({ debug: true, onError: console.error })
 
-app.use(({ req, res }, next) => {
+app.use('*', async ({ req, res }, next) => {
 	// Set the allowed origin
 	res.headers.set('Access-Control-Allow-Origin', 'https://extensions.shopifycdn.com')
 	// Set the allowed methods
@@ -21,10 +23,54 @@ app.use(({ req, res }, next) => {
 	if (req.method === 'OPTIONS') {
 		return { status: 204 }
 	}
-	next()
+	return await next()
 })
 
-app.use('/humanize-product', humanizeProduct)
+app.all('/humanize-product', async ({ req, res }) => {
+	if (req.method.toUpperCase() === 'GET') {
+		return {
+			message: 'Method not allowed',
+			status: 405,
+			usage: {
+				method: 'POST',
+				body: {
+					description_html: 'string',
+					keywords: 'string',
+					prompt: 'string',
+					selected_voice: 'string',
+					specs: 'string',
+					title: 'string',
+				},
+			},
+		}
+	} else if (req.method.toUpperCase() === 'POST') {
+		const { prompt, title, specs, description_html, selected_voice, keywords } =
+			(await req.json()) as {
+				prompt: string
+				title: string
+				specs: string
+				description_html: string
+				selected_voice: string
+				keywords: string
+			}
+
+		if (!title || !specs || !keywords || !selected_voice || !description_html) {
+			return { message: 'Provide all required fields', status: 401 }
+		}
+
+		const productData = new ProductData({
+			title: title!,
+			specs: specs!,
+			description_html: description_html!,
+			selected_voice: selected_voice!,
+			keywords: keywords!,
+		})
+		const response = await upcellenceAI.humanizeProduct(prompt, productData)
+		return productSchema.parse(
+			JSON.parse(response.text ?? JSON.stringify({ ...response.candidates?.[0] }))
+		)
+	}
+})
 
 app.get('/', async (e) => {
 	return { message: 'Hello World' }
